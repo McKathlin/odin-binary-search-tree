0ba22739edefd2dffcba0e89be0c70baf56766a8
McKathlin <mckathlin@gmail.com>
2025-01-04 23:41:00 +0000

    Add tree.js as stub

---
+++ tree.js
@@ -0,0 +1,23 @@
+export default (function() {
+  class Tree {
+    constructor(arr) {
+      this._root = this.buildTree(arr);
+    }
+    
+    buildTree(arr) {
+      // TODO
+    }
+
+    // TODO
+  }
+
+  class Node {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+
+    // TODO: Anything else needed here?
+  }
+}());
