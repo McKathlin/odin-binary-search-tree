5e70133ed939e68ec28f3ec999c410a38cdd1a01
McKathlin <mckathlin@gmail.com>
2025-01-06 19:20:39 +0000

    Add buildTree and prettyPrint

--- tree.js
+++ tree.js
@@ -1,14 +1,35 @@
 export default (function() {
   class Tree {
     constructor(arr) {
-      this._root = this.buildTree(arr);
+      const sortedArr = arr.toSorted((a, b) => a - b);
+      this._root = this.buildTree(sortedArr, 0, sortedArr.length - 1);
     }
     
-    buildTree(arr) {
-      // TODO
+    buildTree(sortedArr, firstIndex, lastIndex) {
+      if (lastIndex < firstIndex) {
+        return null;
+      }
+      const middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+      const root = new Node(sortedArr(middleIndex));
+      root.left = buildTree(sortedArr, firstIndex, middleIndex - 1);
+      root.right = buildTree(sortedArr, middleIndex + 1, lastIndex);
+      return root;
     }
 
-    // TODO
+    // prettyPrint code courtesy of The Odin Project
+    prettyPrint(node, prefix = "", isLeft = true) {
+      if (node === null) {
+        return;
+      }
+      if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      }
+      console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+      if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      }
+    };
+   
   }
 
   class Node {
