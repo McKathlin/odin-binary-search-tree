d62e6ab0f4ebd4d6e00d65cd7456b8e636e64e69
McKathlin <mckathlin@gmail.com>
2025-01-06 19:47:31 +0000

    Test tree creation on a variety of integer arrays

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("This is main.js");
+import Tree from "./tree.js";
+
+let emptyTree = new Tree([]);
+emptyTree.prettyPrint();
+
+let singleNodeTree = new Tree([42]);
+singleNodeTree.prettyPrint();
+
+let simpleTree = new Tree([10, 30, 20]);
+simpleTree.prettyPrint();
+
+let biggerTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+biggerTree.prettyPrint();
