6d577d2ee77f42de4715c40413a32164e1715b1a
McKathlin <mckathlin@gmail.com>
2025-01-06 19:47:46 +0000

    Fix bugs and remove duplicate elements

--- tree.js
+++ tree.js
@@ -1,7 +1,8 @@
 export default (function() {
   class Tree {
-    constructor(arr) {
-      const sortedArr = arr.toSorted((a, b) => a - b);
+    constructor(arr = []) {
+      // The source array must be duplicate-free and sorted.
+      const sortedArr = Array.from(new Set(arr)).sort((a, b) => a - b);
       this._root = this.buildTree(sortedArr, 0, sortedArr.length - 1);
     }
     
@@ -10,23 +11,25 @@ export default (function() {
         return null;
       }
       const middleIndex = Math.floor((lastIndex + firstIndex) / 2);
-      const root = new Node(sortedArr(middleIndex));
-      root.left = buildTree(sortedArr, firstIndex, middleIndex - 1);
-      root.right = buildTree(sortedArr, middleIndex + 1, lastIndex);
+      const root = new Node(sortedArr[middleIndex]);
+      root.left = this.buildTree(sortedArr, firstIndex, middleIndex - 1);
+      root.right = this.buildTree(sortedArr, middleIndex + 1, lastIndex);
       return root;
     }
 
+
+
     // prettyPrint code courtesy of The Odin Project
-    prettyPrint(node, prefix = "", isLeft = true) {
+    prettyPrint(node = this._root, prefix = "", isLeft = true) {
       if (node === null) {
         return;
       }
       if (node.right !== null) {
-        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
       }
       console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
       if (node.left !== null) {
-        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
       }
     };
    
@@ -34,11 +37,13 @@ export default (function() {
 
   class Node {
     constructor(value) {
-      this.value = value;
+      this.data = value;
       this.left = null;
       this.right = null;
     }
 
     // TODO: Anything else needed here?
   }
+
+  return Tree;
 }());
